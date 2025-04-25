5c1e666e0a9dde61e49537008e9aff174cb7219f
KevenTheCreator <keven.santos1@outlook.com>
2025-04-24 21:38:15 -0300

    configuração de mostrar senha criada com sucesso

--- front-end/src/components/login/login.jsx
+++ front-end/src/components/login/login.jsx
@@ -1,4 +1,4 @@
-import { FaEnvelope, FaLock } from "react-icons/fa";
+import { FaEnvelope, FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
@@ -7,6 +7,7 @@ import "./login.css";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
@@ -18,23 +19,16 @@ const Login = () => {
         password,
       });
 
-      console.log("Resposta da API: ", response);
-
       if (response.data && response.data.token && response.data.refreshToken) {
         const { token, refreshToken } = response.data;
-
-        console.log("token: ", token);
-        console.log("refreshToken: ", refreshToken);
 
         localStorage.setItem("token", token);
         localStorage.setItem("refreshToken", refreshToken);
 
-        navigate("/tela-inicial"); 
-
+        navigate("/tela-inicial");
       } else {
         setErrorMessage("Falha ao autenticar. Resposta inesperada.");
       }
-      
     } catch (error) {
       console.log(error);
       setErrorMessage("Falha ao autenticar. Verifique suas credenciais.");
@@ -67,9 +61,10 @@ const Login = () => {
   }, []);
 
   return (
-    <div className="container">     
+    <div className="container">
       <form onSubmit={handleLogin}>
         <h1>Portal Admin</h1>
+
         <div className="input-field">
           <input
             type="email"
@@ -80,20 +75,36 @@ const Login = () => {
           />
           <FaEnvelope className="icon" />
         </div>
+
         <div className="input-field">
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             placeholder="Senha"
             required
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <FaLock className="icon" />
+          {showPassword ? (
+            <FaEyeSlash
+              id="eye"
+              className="icon"
+              onClick={() => setShowPassword(false)}
+            />
+          ) : (
+            <FaEye
+              id="eye"
+              className="icon"
+              onClick={() => setShowPassword(true)}
+            />
+          )}
         </div>
+
         <div className="recall-forget">
           <Link to="/esqueceuSenha">Esqueceu a senha?</Link>
         </div>
+
         <button type="submit">Entrar</button>
+
         {errorMessage && <div className="error-message">{errorMessage}</div>}
       </form>
     </div>
