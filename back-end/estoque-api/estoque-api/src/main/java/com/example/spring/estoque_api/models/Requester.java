f3e30f03ac2d8107c930f80faa82d92a05a2e7a2
KevenTheCreator <keven.santos1@outlook.com>
2025-06-24 21:47:05 -0300

    revert: desfazendo as alterações do commit Criando funcionalidades de saída

--- back-end/estoque-api/estoque-api/src/main/java/com/example/spring/estoque_api/models/Requester.java
+++
@@ -1,5 +0,0 @@
-package com.example.spring.estoque_api.models;
-
-public class Requester {
-
-}
--- back-end/estoque-api/estoque-api/src/main/java/com/example/spring/estoque_api/repositories/DispatchRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring.estoque_api.repositories;
-
-import com.example.spring.estoque_api.models.Dispatch;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DispatchRepository extends JpaRepository<Dispatch, Long> {
-}
