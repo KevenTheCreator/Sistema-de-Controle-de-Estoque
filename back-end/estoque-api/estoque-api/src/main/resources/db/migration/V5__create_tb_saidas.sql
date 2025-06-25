77a340af5df10631e5c5177111397495a8beb6d3
KevenTheCreator <keven.santos1@outlook.com>
2025-06-24 23:47:11 -0300

    mudanças realizadas em nas configurações da aplicação e na migration tb_saidas

--- back-end/estoque-api/estoque-api/src/main/resources/db/migration/V5__create_tb_saidas.sql
+++ back-end/estoque-api/estoque-api/src/main/resources/db/migration/V5__create_tb_saidas.sql
@@ -2,10 +2,10 @@ CREATE TABLE tb_saidas (
     id BIGSERIAL PRIMARY KEY,
     produto VARCHAR(255) NOT NULL,
     quantidade_retirada INT NOT NULL,
-    data_de_saida DATE NOT NULL,
-    data_de_devolucao DATE,
+    data_saida DATE NOT NULL,
+    data_devolucao DATE,
     nome_solicitante VARCHAR(255) NOT NULL,
-    tipo_do_solicitante VARCHAR(255) NOT NULL,
+    tipo_solicitante VARCHAR(255) NOT NULL,
     destino VARCHAR(255) NOT NULL,
     status VARCHAR(255)
 );
